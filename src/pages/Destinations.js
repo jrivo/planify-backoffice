02dbbc305a41a3038d9cece4770743cb29b24cce
amin <aminwastaken@gmail.com>
2022-07-28 13:11:37 +0200

    Create the dashboard's base template

---
+++ src/pages/Destinations.js
@@ -0,0 +1,10 @@
+import { Typography, Box } from "@mui/material";
+
+const Destinations = () => {
+  return (
+    <Box>
+      <Typography>Destinations</Typography>
+    </Box>
+  );
+};
+export default Destinations;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import { Typography, Box } from "@mui/material";
+
+const Home = () => {
+  return (
+    <Box>
+      <Typography>Home</Typography>
+    </Box>
+  );
+};
+export default Home;
